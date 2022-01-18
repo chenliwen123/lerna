0ec37c1e9bd3d757254b99641ea69f034550fa6f
chenliwen_hand <1410762621@qq.com>
2022-01-18 16:48:41 +0800

    修改包名

---
+++ packages/gpwebpack12/package/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gpwebpack12">
+    gpwebpack12
+  </div>
+</template>
+
+<script>
+export default {
+  name:'gpwebpack12',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
